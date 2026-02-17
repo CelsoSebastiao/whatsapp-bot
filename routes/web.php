65a9cdde9eaa0502f874cf08b5ea9dcb0470a64b
CelsoSebastiao <142792582+CelsoSebastiao@users.noreply.github.com>
2026-02-17 16:04:24 +0200

    kkhj

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
+use Illuminate\Support\Facades\Http;
 
-use Illuminate\Support\Facades\Route;
+Route::get('/teste-whatsapp', function() {
+    $token = env('WHATSAPP_TOKEN');
+    $phoneId = env('WHATSAPP_PHONE_ID');
+    $numeroDestino = '258841509766'; // número real do destinatário
 
-Route::get('/', function () {
-    return view('welcome');
+    $response = Http::withHeaders([
+        'Authorization' => 'Bearer ' . $token,
+        'Content-Type' => 'application/json',
+    ])->post("https://graph.facebook.com/v17.0/$phoneId/messages", [
+        'messaging_product' => 'whatsapp',
+        'to' => $numeroDestino,
+        'type' => 'text',
+        'text' => ['body' => 'Olá! Teste de mensagem via WhatsApp API.']
+    ]);
+
+    return $response->json();
 });
+
+
